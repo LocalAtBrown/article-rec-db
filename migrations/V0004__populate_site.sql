ae49f1b58d91a22d308d33f716fbf7ed4fcd35d8
Sylvan Zheng <squidgetx@gmail.com>
2021-10-18 23:04:35 -0400

    fix typo

--- migrations/V0004__populate_site.sql
+++ migrations/V0004__populate_site.sql
@@ -3,6 +3,6 @@
 -- the site parameter properly. After this change,
 -- make the site parameter required not null
 
-UPDATE TABLE article SET site = 'washington-city-paper';
+UPDATE article SET site = 'washington-city-paper' where site is null;
 
 ALTER TABLE article ALTER COLUMN site SET NOT NULL; 
