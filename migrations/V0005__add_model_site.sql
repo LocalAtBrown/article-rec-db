e8716b88ce04546df0408368174af725b50a36f7
ting zhang <thetingzhang@gmail.com>
2021-11-23 11:56:18 -0500

    [Fix] typo

--- migrations/V0005__add_model_site.sql
+++ migrations/V0005__add_model_site.sql
@@ -1,5 +1,5 @@
 DROP INDEX idx_type_status;
 
-ALTER TABLE article ADD site TEXT NOT NULL DEFAULT '';
+ALTER TABLE model ADD site TEXT NOT NULL DEFAULT '';
 
 CREATE INDEX idx_type_status_site ON model (type, status, site);
