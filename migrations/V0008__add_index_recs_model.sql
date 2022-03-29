e0a6ddeaa472500c21c9e408e77503ec0e1ec8cd
ting zhang <thetingzhang@gmail.com>
2022-03-29 12:13:05 -0400

    [update] migration must be idempotent in this case

--- migrations/V0008__add_index_recs_model.sql
+++ migrations/V0008__add_index_recs_model.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_rec_model_id ON recommendation (model_id);
-CREATE INDEX idx_rec_article_id ON recommendation (recommended_article_id);
+CREATE INDEX IF NOT EXISTS idx_rec_model_id ON recommendation (model_id);
+CREATE INDEX IF NOT EXISTS idx_rec_article_id ON recommendation (recommended_article_id);
