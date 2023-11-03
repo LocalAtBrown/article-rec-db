b2464e5b2e7a69ba468f9b289c24c751bdf6b0f6
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-11-03 12:06:15 -0400

    [add] site objects

---
+++ article_rec_db/sites/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["AFRO_LA", "DALLAS_FREE_PRESS"]
+
+from .sites import AFRO_LA, DALLAS_FREE_PRESS
---
+++ article_rec_db/sites/sites.py
@@ -0,0 +1,4 @@
+from .base import Site, SiteName
+
+AFRO_LA = Site(name=SiteName.AFRO_LA)
+DALLAS_FREE_PRESS = Site(name=SiteName.DALLAS_FREE_PRESS)
