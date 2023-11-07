6a5533a7c8d5f75e46571105661422b8c373ec07
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-11-06 23:54:46 -0500

    [add] duplicate site and site id test

--- tests/integration/test_models.py
+++ tests/integration/test_models.py
@@ -133,21 +133,46 @@ def test_add_article_without_page(create_and_drop_tables, engine):
         assert num_articles == 0
 
 
-# @pytest.mark.order(8)
-# def test_add_article_excluded(create_and_drop_tables, engine):
-#     page = Page(
-#         url="https://afrolanews.org/",
-#         article_exclude_reason=ArticleExcludeReason.NOT_ARTICLE,
-#     )
-#     with Session(engine) as session, pytest.raises(
-#         ArticleValidationError,
-#         match=r"cannot be added to the article table because it's excluded as specified in the page table",
-#     ):
-#         article = Article(
-#             session=session,
-#             site=AFRO_LA.name,
-#             id_in_site="1111",
-#             title="Home page and definitely not an article",
-#             published_at=datetime.now(),
-#             page=page,
-#         )
+@pytest.mark.order(8)
+def test_add_article_excluded(create_and_drop_tables, engine):
+    # Would be nice to test that adding a non-article (whose page has a non-null article_exclude_reason)
+    # to the article table fails, but doing so at the model level is messy, so for now passing the responsibility
+    # to the instance creation method in whichever application that uses this library
+    pass
+
+
+@pytest.mark.order(9)
+def test_add_articles_duplicate_site_and_id_in_site(create_and_drop_tables, engine):
+    page1 = Page(
+        url="https://dallasfreepress.com/example-article/",
+        article_exclude_reason=None,
+    )
+    page2 = Page(
+        url="https://dallasfreepress.com/example-article-2/",
+        article_exclude_reason=None,
+    )
+    id_in_site = "1234"
+    article1 = Article(
+        site=DALLAS_FREE_PRESS.name,
+        id_in_site=id_in_site,
+        title="Example Article",
+        published_at=datetime.now(),
+        page=page1,
+    )
+    article2 = Article(
+        site=DALLAS_FREE_PRESS.name,
+        id_in_site=id_in_site,
+        title="Example Article 2",
+        published_at=datetime.now(),
+        page=page2,
+    )
+
+    with Session(engine) as session:
+        session.add(article1)
+        session.add(article2)
+        # Since the combination of site and id_in_site is unique, adding an article with an already existing site and id_in_site must fail
+        with pytest.raises(
+            IntegrityError,
+            match=r"duplicate key value violates unique constraint \"article_site_id_in_site_key\"",
+        ):
+            session.commit()
