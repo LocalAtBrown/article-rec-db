5bcba117c3564103672d631b679aebbcc4cf4164
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-11-17 13:56:49 -0500

    [add] training job permissions

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,12 +1,27 @@
 locals {
   sites                  = toset(var.sites)
+  schema                 = "public"
   password_special_chars = "!@$"
   tables = {
-    "page"           = {}
-    "article"        = {}
-    "execution"      = {}
-    "embedding"      = {}
-    "recommendation" = {}
+    "page"           = "page"
+    "article"        = "article"
+    "execution"      = "execution"
+    "embedding"      = "embedding"
+    "recommendation" = "recommendation"
+  }
+}
+
+locals {
+  training_job_privileges = {
+    "table" = {
+      "SELECT" = keys(local.tables)
+      "INSERT" = keys(local.tables)
+      "UPDATE" = [local.tables.page, local.tables.article]
+      "DELETE" = keys(local.tables)
+    }
+  }
+  reader_privileges = {
+    "table" = { "SELECT" = keys(local.tables) }
   }
 }
 
@@ -25,6 +40,17 @@ resource "postgresql_extension" "pgvector" {
 resource "postgresql_role" "training_job" {
   name  = "${var.stage}_training_job"
   login = false
+}
+
+resource "postgresql_grant" "training_job_table_privileges" {
+  for_each = local.training_job_privileges.table
+
+  database    = postgresql_database.db.name
+  role        = postgresql_role.training_job.name
+  schema      = local.schema
+  object_type = "table"
+  objects     = each.value
+  privileges  = [each.key]
 }
 
 resource "random_password" "training_job_site_password" {
@@ -71,13 +97,15 @@ resource "postgresql_role" "reader" {
   password = random_password.reader_password.result
 }
 
-resource "postgresql_grant" "readonly_tables" {
+resource "postgresql_grant" "reader_table_privileges" {
+  for_each = local.reader_privileges.table
+
   database    = postgresql_database.db.name
   role        = postgresql_role.reader.name
-  schema      = "public"
+  schema      = local.schema
   object_type = "table"
-  objects     = keys(local.tables)
-  privileges  = ["SELECT"]
+  objects     = each.value
+  privileges  = [each.key]
 }
 
 resource "aws_ssm_parameter" "reader_credentials" {
