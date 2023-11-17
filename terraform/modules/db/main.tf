47ed8ead78189a94c9015c0d014f84effbbddafe
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-11-17 12:52:44 -0500

    [add] readonly user

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,9 +1,24 @@
 locals {
-  sites = toset(var.sites)
+  sites                  = toset(var.sites)
+  password_special_chars = "!@$"
+  tables = {
+    "page"           = {}
+    "article"        = {}
+    "execution"      = {}
+    "embedding"      = {}
+    "recommendation" = {}
+  }
 }
 
 resource "postgresql_database" "db" {
   name = var.stage
+}
+
+# Enable extensions
+resource "postgresql_extension" "pgvector" {
+  name     = "vector"
+  version  = "0.4.1"
+  database = postgresql_database.db.name
 }
 
 # Training job role and site users
@@ -17,7 +32,7 @@ resource "random_password" "training_job_site_password" {
 
   length           = 20
   special          = true
-  override_special = "!@$"
+  override_special = local.password_special_chars
 }
 
 resource "postgresql_role" "training_job_site" {
@@ -43,9 +58,36 @@ resource "aws_ssm_parameter" "training_job_site_credentials" {
   })
 }
 
-# Enable extensions
-resource "postgresql_extension" "pgvector" {
-  name     = "vector"
-  version  = "0.4.1"
-  database = postgresql_database.db.name
+# Read-only role and user
+resource "random_password" "reader_password" {
+  length           = 20
+  special          = true
+  override_special = local.password_special_chars
+}
+
+resource "postgresql_role" "reader" {
+  name     = "${var.stage}_reader"
+  login    = true
+  password = random_password.reader_password.result
+}
+
+resource "postgresql_grant" "readonly_tables" {
+  database    = postgresql_database.db.name
+  role        = postgresql_role.reader.name
+  schema      = "public"
+  object_type = "table"
+  objects     = keys(local.tables)
+  privileges  = ["SELECT"]
+}
+
+resource "aws_ssm_parameter" "reader_credentials" {
+  name = "/${var.stage}/article-rec/reader/all/database-credentials"
+  type = "String"
+  value = jsonencode({
+    HOST     = var.host
+    PORT     = var.port
+    DB_NAME  = var.stage
+    USERNAME = postgresql_role.reader.name
+    PASSWORD = random_password.reader_password.result
+  })
 }
