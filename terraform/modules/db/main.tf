4473c8fd6b9fe51bebb57758ec9e758d96fd52a5
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-11-17 14:15:57 -0500

    Add table privileges (#15)
    * [add] readonly user
    
    * [add] training job permissions

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,9 +1,39 @@
 locals {
-  sites = toset(var.sites)
+  sites                  = toset(var.sites)
+  schema                 = "public"
+  password_special_chars = "!@$"
+  tables = {
+    "page"           = "page"
+    "article"        = "article"
+    "execution"      = "execution"
+    "embedding"      = "embedding"
+    "recommendation" = "recommendation"
+  }
+}
+
+locals {
+  training_job_privileges = {
+    "table" = {
+      "SELECT" = keys(local.tables)
+      "INSERT" = keys(local.tables)
+      "UPDATE" = [local.tables.page, local.tables.article]
+      "DELETE" = keys(local.tables)
+    }
+  }
+  reader_privileges = {
+    "table" = { "SELECT" = keys(local.tables) }
+  }
 }
 
 resource "postgresql_database" "db" {
   name = var.stage
+}
+
+# Enable extensions
+resource "postgresql_extension" "pgvector" {
+  name     = "vector"
+  version  = "0.4.1"
+  database = postgresql_database.db.name
 }
 
 # Training job role and site users
@@ -12,12 +42,23 @@ resource "postgresql_role" "training_job" {
   login = false
 }
 
+resource "postgresql_grant" "training_job_table_privileges" {
+  for_each = local.training_job_privileges.table
+
+  database    = postgresql_database.db.name
+  role        = postgresql_role.training_job.name
+  schema      = local.schema
+  object_type = "table"
+  objects     = each.value
+  privileges  = [each.key]
+}
+
 resource "random_password" "training_job_site_password" {
   for_each = local.sites
 
   length           = 20
   special          = true
-  override_special = "!@$"
+  override_special = local.password_special_chars
 }
 
 resource "postgresql_role" "training_job_site" {
@@ -43,9 +84,38 @@ resource "aws_ssm_parameter" "training_job_site_credentials" {
   })
 }
 
-# Enable extensions
-resource "postgresql_extension" "pgvector" {
-  name     = "vector"
-  version  = "0.4.1"
-  database = postgresql_database.db.name
+# Read-only role and user
+resource "random_password" "reader_password" {
+  length           = 20
+  special          = true
+  override_special = local.password_special_chars
+}
+
+resource "postgresql_role" "reader" {
+  name     = "${var.stage}_reader"
+  login    = true
+  password = random_password.reader_password.result
+}
+
+resource "postgresql_grant" "reader_table_privileges" {
+  for_each = local.reader_privileges.table
+
+  database    = postgresql_database.db.name
+  role        = postgresql_role.reader.name
+  schema      = local.schema
+  object_type = "table"
+  objects     = each.value
+  privileges  = [each.key]
+}
+
+resource "aws_ssm_parameter" "reader_credentials" {
+  name = "/${var.stage}/article-rec/reader/all/database-credentials"
+  type = "String"
+  value = jsonencode({
+    HOST     = var.host
+    PORT     = var.port
+    DB_NAME  = var.stage
+    USERNAME = postgresql_role.reader.name
+    PASSWORD = random_password.reader_password.result
+  })
 }
